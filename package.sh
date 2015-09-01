96d3b180e608120b8d68ffc5eefd73f6a17410be
Chris Allison <chris.allison@bgch.co.uk>
2015-09-01 10:36:27 +0100

    add check for files in correct locations before packaging

--- package.sh
+++ package.sh
@@ -12,4 +12,8 @@ mkdir -p build/etc/init.d build/usr/bin
 cp splrgd build/usr/bin/splrgd
 cp splrgd.conf build/etc/splrgd.conf
 cp init-splrgd.sh build/etc/init.d/splrgd
-fpm -s dir -t deb -C build -a amd64 --verbose --name splrg --version 0.9-${iteration} -d libargtable2-0 --config-files /etc/splrgd.conf .
+if [ -r build/etc/init.d/splrgd -a -r build/etc/splrgd.conf -a -r build/usr/bin/splrgd ]; then
+  fpm -s dir -t deb -C build -a amd64 --verbose --name splrg --version 0.9-${iteration} -d libargtable2-0 --config-files /etc/splrgd.conf .
+else
+  echo "failed to build package for some reason or other"
+fi
